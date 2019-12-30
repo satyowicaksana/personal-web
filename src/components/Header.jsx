eb5e7266bc9c9845fc89ef0ed7b10739b926d808
Satyo Wicaksana <satyowicaksana@gmail.com>
2019-12-31 03:25:09 +0700

    header section content

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.scss'
+
+export default () => {
+  return (
+    <div className="header">
+      <div className="container">
+        <p>Hello, World! I'm</p>
+        <h1>Satyo Wicaksana,</h1>
+        <h1 class="fade">Software Engineer.</h1>
+      </div>
+    </div>
+  )
+}
