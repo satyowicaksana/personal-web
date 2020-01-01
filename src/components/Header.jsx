90a4b60f5e7481c0ec3ced71cd9f80ae988408f3
Satyo Wicaksana <satyowicaksana@gmail.com>
2020-01-01 07:56:19 +0700

    styling header responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from 'react'
 import './Header.scss'
+import { Button } from 'antd';
 
 export default () => {
   return (
     <div className="header">
+      <div className="diagonal">
+      </div>
       <div className="container">
         <p>Hello, World! I'm</p>
         <h1>Satyo Wicaksana,</h1>
-        <h1 class="fade">Software Engineer.</h1>
+        <h1 className="fade">Software Engineer.</h1>
       </div>
     </div>
   )
