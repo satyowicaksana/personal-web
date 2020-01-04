a2e943f520e720374f418fc85918ce41c41aab8b
Satyo Wicaksana <satyowicaksana@gmail.com>
2020-01-04 09:18:58 +0700

    profile layout

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Profile.scss'
+import { Avatar, Card, Row, Col, Icon } from 'antd'
 import { fadeInLeft, fadeInDown } from 'react-animations';
 import Radium, {StyleRoot} from 'radium';
 
@@ -18,8 +19,22 @@ export default () => {
   return (
     <StyleRoot>
     <div className="profile">
-      <img src={require('../blob-profile.svg')} alt="blob-profile"></img>
-      <div className="container"></div>
+      <Avatar className="avatar" size={150} src={require('../avatar.jpg')} />
+      <img className="blob-md" src={require('../blob-profile-1.svg')} alt="blob-profile"></img>
+      <img className="blob-sm" src={require('../blob-profile-2.svg')} alt="blob-profile"></img>
+      <Card className="container">
+        <Row>
+          <Col style={{paddingTop: '20px'}} span={12}>
+            <h2>About me.</h2>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </Col>
+          <Col span={12}>
+            <h2>Satyo Wicaksana</h2>
+            <Icon type="calendar" /><span> 19 August 1995</span>
+            <h3>Jakarta, Indonesia</h3>
+          </Col>
+        </Row>
+      </Card>
     </div>
     </StyleRoot>
   )
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header'
 import Profile from '../components/Profile'
+import './Home.scss'
 
 export default () => {
+  const [loading, setLoading] = useState(true)
+  
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 1000)
+  }, [])
+  if(loading) {
+    return (
+      <div className="home">
+        <img src={require('../logo.png')} className="spinner" alt="logo"/>
+      </div>
+    )
+  }
   return (
     <div>
       <Header></Header>
